0afb311203b2129a0dce4e8dc53f1c7a0548436f
RubenKingCP <redruben_king@yahoo.com>
2022-06-14 13:29:40 +0300

    Finish HTML and Css

--- code.js
+++ code.js
@@ -1,9 +1,15 @@
 const yesBtn = document.querySelector('#yesBtn');
+
 const noBtn = document.querySelector('#noBtn');
+
 const startWrapper = document.querySelector('#startWrapper');
+
+const gamePage = document.querySelector('#mainWrapper');
+
 yesBtn.addEventListener('click', () => {
     startWrapper.remove();
 } );
+
 noBtn.addEventListener('click', () => {
     noBtn.remove();
 } );
