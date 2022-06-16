0c72ba6a8c3de283b8f86b22e8e37d2918dfd59d
RubenKingCP <redruben_king@yahoo.com>
2022-06-16 15:56:24 +0300

    Finish Base Code

--- code.js
+++ code.js
@@ -6,6 +6,38 @@ const startWrapper = document.querySelector('#startWrapper');
 
 const gamePage = document.querySelector('#mainWrapper');
 
+const playerImage = document.querySelector("#playerChoice");
+
+const robotImage = document.querySelector("#botChoice");
+
+const rock = document.querySelector("#rock");
+
+const paper = document.querySelector("#paper");
+
+const scissors = document.querySelector("#scissors");
+
+const endScreen = document.querySelector("#endScreen");
+
+const endBtn = document.querySelector("#endBtn");
+
+const endMsg = document.querySelector("#endMessage");
+
+let playerChoice = "";
+
+let robChoice = "";
+
+let playerScore = 0;
+
+let robotScore = 0;
+
+let messageVictory = document.createElement('p');
+
+messageVictory.innerHTML = "Congrats you won";
+
+let messageLose = document.createElement('p');
+
+messageLose.innerHTML = "Oh no you lost."
+
 yesBtn.addEventListener('click', () => {
     startWrapper.remove();
 } );
@@ -15,62 +47,130 @@ noBtn.addEventListener('click', () => {
 } );
 
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-
 rock.addEventListener("click", () => {
+    let item = "rock";
     paper.classList.remove("playerSelection")
     scissors.classList.remove("playerSelection")
     rock.classList.add("playerSelection")
     choiceeee();
     robotChoice();
-    comparison();    
+    setPlayerChoice(item);
+    comparison();
+    endGame();
 });
 
 scissors.addEventListener("click", () => {
-    paper.classList.remove("playerSelection")
-    scissors.classList.add("playerSelection")
-    rock.classList.remove("playerSelection")
+    let item = "scissors";
+    paper.classList.remove("playerSelection");
+    scissors.classList.add("playerSelection");
+    rock.classList.remove("playerSelection");
+    setPlayerChoice(item);
     choiceeee();
     robotChoice();
     comparison();
+    endGame();
 });
 
 paper.addEventListener("click", () => {
+    let item = "paper";
     paper.classList.add("playerSelection")
     scissors.classList.remove("playerSelection")
     rock.classList.remove("playerSelection")
+    setPlayerChoice(item);
     choiceeee();
     robotChoice();
     comparison();
+    console.log(playerScore);
+    endGame();
 });
 
-let playerChoice = "";
+endBtn.addEventListener("click", () => startOver());
+
 function choiceeee() {
 playerChoice = document.querySelector(".playerSelection").value;
 };
 
-let robChoice = "";
 function robotChoice() {
     let randomOne = Math.random();
     if (randomOne < 0.33) {
         robChoice = "rock";
+        let botItem = "rock";
+        setbotChoice(botItem);
     } else if (randomOne < 0.66) {
         robChoice = "paper";
+        let botItem = "paper";
+        setbotChoice(botItem);
     } else if (randomOne <= 1) {
         robChoice = "scissors";
+        let botItem = "scissors";
+        setbotChoice(botItem);
     };
 };
 
 function comparison() {
     if (robChoice == "paper" && playerChoice == "rock") {
-        console.log("hey");
+        robotScore += 1;
+        lightUpBot();
     } else if (robChoice == "scissors" && playerChoice == "paper") {
-        console.log("heyy") 
+        robotScore += 1;
+        lightUpBot();
     } else if (robChoice == "rock" && playerChoice == "scissors") {
-        console.log("heyyyy");
+        robotScore += 1;
+        lightUpBot();
     } else if (robChoice == playerChoice) {
-        console.log("Tie")
-    } else console.log("u win");
+        console.log("tie");
+    } else {
+        playerScore += 1;
+        lightUpPlayer();
+    };
+};
+
+function setPlayerChoice(item) {
+    playerImage.style.backgroundImage = "url(" + "Images/" + item + ".png" + ")";
+    playerImage.style.backgroundRepeat = "no-repeat";
+    playerImage.style.backgroundSize = "contain";
+    playerImage.style.backgroundPosition = "center";
+};
+
+function setbotChoice(item) {
+    robotImage.style.backgroundImage = "url(" + "Images/" + item + ".png" + ")";
+    robotImage.style.backgroundRepeat = "no-repeat";
+    robotImage.style.backgroundSize = "contain";
+    robotImage.style.backgroundPosition = "center";
+};
+
+
+function endGame() {
+    if (playerScore == 3) {
+        endScreen.classList.toggle("hidden");
+        endMsg.appendChild(messageVictory);
+    } else if(robotScore == 3) {
+        endScreen.classList.toggle("hidden");
+        endMsg.appendChild(messageLose);
+    };
+};
+
+function startOver() {
+    playerScore = 0;
+    robotScore = 0;
+    endScreen.classList.toggle("hidden");
+    endMsg.innerHTML = "";
+    reset();
+};
+
+function lightUpPlayer() {
+    document.querySelector("#" + "playerScore" + playerScore + "").classList.add("playerWinBubble");
+}
+
+function lightUpBot() {
+    document.querySelector("#" + "botScore" + robotScore + "").classList.add("botWinBubble");
+}
+
+function reset() {
+    document.querySelector("#playerScore1").classList.remove("playerWinBubble");
+    document.querySelector("#playerScore2").classList.remove("playerWinBubble");
+    document.querySelector("#playerScore3").classList.remove("playerWinBubble");
+    document.querySelector("#botScore1").classList.remove("botWinBubble");
+    document.querySelector("#botScore2").classList.remove("botWinBubble");
+    document.querySelector("#botScore3").classList.remove("botWinBubble");
 };
